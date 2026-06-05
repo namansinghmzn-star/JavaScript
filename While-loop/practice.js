e966002e2d1a19e2487116ff0f6f244c21c84003
Naman Singh <namansinghmzn@gmail.com>
2026-06-05 14:34:22 +0530

    even numbers

--- While-loop/practice.js
+++ While-loop/practice.js
@@ -4,8 +4,14 @@
 //     i++
 // }  
 
-let i =5;
-while(i>=1){
+// let i =5;
+// while(i>=1){
+//     console.log(i)
+//     i--
+// }
+
+let i=2
+while(i<=20){
     console.log(i)
-    i--
+    i+=2
 }
