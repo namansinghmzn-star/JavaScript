000217c57bbb3cbe49ff1622e7eaf94877cce7d6
Naman Singh <namansinghmzn@gmail.com>
2026-06-05 15:20:52 +0530

    Sum of even numbers from 1-100

--- While-loop/practice.js
+++ While-loop/practice.js
@@ -37,10 +37,22 @@
 // }
 // console.log(total)
 
-let i= 1
+// let i= 1
+// while(i<=100){
+//     if(i%5==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// Find the sum of all even numbers from 1 to 100.
+
+let i = 1;
+let total = 0;
 while(i<=100){
-    if(i%5==0){
-        console.log(i)
+    if(i%2==0){
+        total+=i
     }
     i++
 }
+console.log(total)
