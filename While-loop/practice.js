e36aef61ce9675f87af0ca16cabe8e3d4ee996aa
Naman Singh <namansinghmzn@gmail.com>
2026-06-05 14:50:12 +0530

    Table printing

--- While-loop/practice.js
+++ While-loop/practice.js
@@ -16,8 +16,14 @@
 //     i+=2
 // }
 
-let i= 1 
-while(i<=20){
-    console.log(i)
-    i+=2
+// let i= 1 
+// while(i<=20){
+//     console.log(i)
+//     i+=2
+// }
+
+let n = 3
+let i = 1
+while(i<=1){
+    console.log(n,"X",i,"=",n*i)
 }
