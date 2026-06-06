889a3ce16f764c19788f11221bde6fc03a33201d
Naman Singh <namansinghmzn@gmail.com>
2026-06-06 20:03:02 +0530

    Index and upper lower case

--- String/str.js
+++ String/str.js
@@ -8,5 +8,12 @@
 
 // Finding a character or word in Javascript
 
-let text = "I love my India"
-console.log(text.includes("love"))
+// let text = "I love my India"
+// console.log(text.includes("love"))
+
+
+// Finding index in string
+
+let name = "Naman Singh"
+console.log(name.indexOf("Singh"))
+console.log(name.toUpperCase())
