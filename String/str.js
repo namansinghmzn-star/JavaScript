1284f1a9ff1d589e3f0134e03ec29c58e4bd4c0d
Naman Singh <namansinghmzn@gmail.com>
2026-06-06 20:41:21 +0530

    Operations on string

--- String/str.js
+++ String/str.js
@@ -14,7 +14,30 @@
 
 // Finding index in string
 
-let name = "Naman Singh"
-console.log(name.indexOf("Singh"))
-console.log(name.toUpperCase())
-console.log(name.toLowerCase())
+// let name = "Naman Singh"
+// console.log(name.indexOf("Singh"))
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+
+
+// length of Text
+
+// let name = "Naman Singh"
+// console.log(name.length)
+// console.log(name[0])
+// console.log(name[7])
+
+
+// Extracting part of string
+
+let text = "SmartPhone"
+console.log(text.slice(0,5))
+let result = text.replace("Smart","Hand")
+console.log(result)
+
+// using trim
+
+// name = "   Naman Singh    "
+// console.log(name)
+// console.log(name.trim())
+
