817f0802a21e56040f75fdab5c6922c067066f0c
Naman Singh <namansinghmzn@gmail.com>
2026-06-07 20:49:48 +0530

    OddEven function

--- function/func.js
+++ function/func.js
@@ -5,3 +5,12 @@
 // }
 // sayHello()
 
+function oddEven(){
+    let num = Number(prompt("Enter the Number: "))
+    if(num%2==0){
+        console.log("Even")
+    }else{
+        console.log("Odd")
+    }
+}
+oddEven()
