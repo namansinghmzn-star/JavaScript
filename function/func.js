5b25ef6cbbd5a87640e3367d75c56c5656eb36e1
Naman Singh <namansinghmzn@gmail.com>
2026-06-08 12:54:51 +0530

    Practicing Functions

--- function/func.js
+++ function/func.js
@@ -39,3 +39,28 @@
 //     console.log("Hey",country)
 // }
 // sayHey("India")
+
+// function showNumber(number){
+//     console.log(number)
+// }
+// showNumber(100)
+// showNumber(1020)
+
+// function addition(a,b){
+//     console.log(a+b)
+// }
+// addition(13,12)
+// addition(10,25)
+
+// function mult(a,b){
+//     console.log("The multplication of",a,"and",b,"is",a*b)
+// }
+// mult(4,5)
+
+// function add(a,b){
+//     console.log("The addtion of",a,"and",b,"is:",a+b)
+// }
+// let a = Number(prompt("Enter the number: "))
+// let b = Number(prompt("Enter the number: "))
+
+// add(a,b)
