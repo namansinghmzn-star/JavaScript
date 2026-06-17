4f118c7909f2a478de1d0b20cda54958f0d524fb
Naman Singh <namansinghmzn@gmail.com>
2026-06-18 01:56:02 +0530

    Basics of Array

--- Array/arr.js
+++ Array/arr.js
@@ -1,4 +1,22 @@
 // Creating a array
 
-let names = ["Naman","Rohan","Vivek"]
-console.log(names)
+// let names = ["Naman","Rohan","Vivek"]
+// console.log(names)
+
+// let num = [1,2,3,4,5,6]
+// console.log(num)
+
+// Mixed array
+// let arr = [1,"Naman",5,"Singh"]
+// console.log(arr)
+// console.log(arr[1])
+
+// Calling element by it's index
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// console.log(arr[5])
+
+// Changing elements of array 
+// let arr = ["Naman","Rohan","Vivek","Divyansh"]
+// arr[3] = "Ayush"
+// console.log(arr)
+// console.log(arr.length)
