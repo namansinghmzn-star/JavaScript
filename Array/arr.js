ca94362b70d312f41db1903281b0743f0894d211
Naman Singh <namansinghmzn@gmail.com>
2026-06-18 16:27:31 +0530

    javascript array practice

--- Array/arr.js
+++ Array/arr.js
@@ -16,7 +16,25 @@
 // console.log(arr[5])
 
 // Changing elements of array 
-let arr = ["Naman","Rohan","Vivek","Divyansh"]
-arr[3] = "Ayush"
-console.log(arr)
-console.log(arr.length)
+// let arr = ["Naman","Rohan","Vivek","Divyansh"]
+// arr[3] = "Ayush"
+// console.log(arr)
+// console.log(arr.length)
+
+// Loop in Arrya
+// let arr = ["Naman","Rohan","Vivek","Divyansh"]
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// let arr = [1,2,3,4,5]
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+let sum = 0
+for(let i=0; i<arr.length;i++){
+    sum = sum+arr[i]
+}
+console.log(sum)
