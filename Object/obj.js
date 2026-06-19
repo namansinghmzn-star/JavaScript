a0159c6dcbe1aea2f4f3d028e1b5710f5e07c93b
Naman Singh <namansinghmzn@gmail.com>
2026-06-19 16:45:38 +0530

    Object, Loop and Function inside array

--- Object/obj.js
+++ Object/obj.js
@@ -36,27 +36,120 @@
 //     marks: 78
 // }
 
+// let student = [
+//     {
+//         name: "Naman",
+//         age:23,
+//         marks:97
+//     },
+//     {
+//         name: "Veepanshu",
+//         age: 21,
+//         marks: 94
+//     },
+//     {
+//         name: "Deepak",
+//         age: 32,
+//         marks:87
+//     }
+// ]
+// for (i=0;i<student.length;i++){
+//     console.log(student[i].name)
+//     console.log("Age",student[i].age)
+//     console.log("Marks",student[i].marks)
+
+// }
+
+// function showStudents(student) {
+//     for (let i = 0; i < student.length; i++) {
+//         console.log(student[i]);
+//     }
+// }
+// showStudents(student)
+
+// student.push({
+//     name: "Rohan",
+//     age: 19,
+//     marks: 72
+// })
+
+// for (i=0;i<student.length;i++){
+//     console.log(student[i].age)
+// }
+
+// let student = [
+//     {
+//         name: "Naman",
+//         age:23,
+//         marks:97
+//     },
+//     {
+//         name: "Veepanshu",
+//         age: 21,
+//         marks: 94
+//     },
+//     {
+//         name: "Deepak",
+//         age: 32,
+//         marks:87
+//     }
+// ]
+// let total = 0
+// for (let i=0; i<student.length;i++){
+//     total = total + student[i].marks
+// }
+// console.log(total)
+
+// let student = [
+//     {
+//         name: "Naman",
+//         age:23,
+//         marks:97
+//     },
+//     {
+//         name: "Veepanshu",
+//         age: 21,
+//         marks: 94
+//     },
+//     {
+//         name: "Deepak",
+//         age: 32,
+//         marks:87
+//     }
+// ]
+// let average = 0
+// let total = 0
+// for (let i=0; i<student.length;i++){
+//     average = total+=student[i].marks/student.length
+// }
+// console.log(average)
+
 let student = [
-    {
-        name: "Naman",
-        age:23,
-        marks:97
-    },
-    {
-        name: "Veepanshu",
-        age: 21,
-        marks: 94
-    },
-    {
-        name: "Deepak",
-        age: 32,
-        marks:87
-    }
-]
-for (i=0;i<student.length;i++){
-    console.log(student[i].name)
-    console.log("Age",student[i].age)
-    console.log("Marks",student[i].marks)
-
-
+  {
+    name: "Naman",
+    age: 23,
+    marks: 97,
+  },
+  {
+    name: "Veepanshu",
+    age: 21,
+    marks: 94,
+  },
+  {
+    name: "Deepak",
+    age: 32,
+    marks: 87,
+  },
+];
+let highest = 0;
+let secondHighest = 0;
+for (let i = 0; i < student.length; i++) {
+  if (student[i].marks > highest) {
+    highest = student[i].marks;
+  } else if (student[i].marks > secondHighest && student[i].marks < highest) {
+    secondHighest = student[i].marks;
+  }
 }
+
+// console.log(highest)
+console.log(secondHighest);
