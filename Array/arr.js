e938a1983d5e925c3cf7831373b960ba6f8eb563
Naman Singh <namansinghmzn@gmail.com>
2026-06-19 12:34:59 +0530

    Array Method Practice

--- Array/arr.js
+++ Array/arr.js
@@ -32,10 +32,37 @@
 //     console.log(arr[i])
 // }
 
-let arr = [1,2,3,4,5,6,7,8,9,10]
-let sum = 0
-for(let i=0; i<arr.length;i++){
-    sum = sum+arr[i]
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let sum = 0
+// for(let i=0; i<arr.length;i++){
+//     sum = sum+arr[i]
+// }
+// console.log(sum)
+// console.log("Naman");
+
+// Array Methods
+// let fruits = ["Banana","Apple"]
+// fruits.push("Mango")
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+
+// let fruits = ["Apple","Banana","Grapes","Orange"]
+// console.log(fruits)
+// fruits.shift()
+// console.log(fruits)
+// fruits.unshift("Mango")
+// console.log(fruits)
+
+// let nam = ["Naman","Veepanshu","Deepak","Sumit"]
+// console.log(nam.includes("Veepanshu"))
+// console.log(nam.indexOf("Naman"))
+// console.log(nam)
+// console.log(nam.join())
+
+let nam = ["Naman","Veepanshu","Deepak","Sumit"]
+if (nam.includes("Naman")){
+    console.log("Name Found")
+}else{
+    console.log("Name Not Found")
 }
-console.log(sum)
-console.log("Naman");
