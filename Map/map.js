f66ac8f9ddc23d236d2055f3ef49258f28515ac8
Naman Singh <namansinghmzn@gmail.com>
2026-06-23 01:29:57 +0530

    Map function in JS

---
+++ Map/map.js
@@ -0,0 +1,20 @@
+// const number = [1,2,3,4,5]
+// const doubled = number.map(num=>num*2)
+// console.log(doubled)
+
+// const names = ["Naman","Veepanshu","Rohan"]
+// const upper = names.map(up=>up.toUpperCase())
+// console.log(upper)
+
+
+// const number = [10,20,45,32]
+// const updated = number.map(update => update+10)
+// console.log(updated)
+
+// const users = [
+//     {name: "Rahul"},
+//     {name: "Amit"},
+//     {name: "Naman"}
+// ];
+// const name = users.map(nam=> nam.name)
+// console.log(name)
