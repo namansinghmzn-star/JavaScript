73d19d7606b9e8aaa6a69863accacbeae9fef3b1
Naman Singh <namansinghmzn@gmail.com>
2026-06-26 11:53:01 +0530

    More Filter Practice

--- Filter/filter.js
+++ Filter/filter.js
@@ -1,32 +1,71 @@
-// Filter Even Number
+// // Filter Even Number
 
-// const arr = [1,2,3,4,5,6,7,8,9,10]
+// // const arr = [1,2,3,4,5,6,7,8,9,10]
 
-// const evenNumber = arr.filter(n => n%2==0)
-// console.log(evenNumber)
+// // const evenNumber = arr.filter(n => n%2==0)
+// // console.log(evenNumber)
 
-// Filter odd number
+// // Filter odd number
 
-// const arr = [1,2,3,4,5,6,7,8,9,10]
-// const oddNumber = arr.filter(n=> n%2==1)
-// console.log(oddNumber)
+// // const arr = [1,2,3,4,5,6,7,8,9,10]
+// // const oddNumber = arr.filter(n=> n%2==1)
+// // console.log(oddNumber)
 
-// Filter number greater than 10 
+// // Filter number greater than 10 
 
-// const numbers = [ 39, 65,1,5,87,34,10,43]
-// const greater = numbers.filter(n=> n>10)
-// console.log(greater)
+// // const numbers = [ 39, 65,1,5,87,34,10,43]
+// // const greater = numbers.filter(n=> n>10)
+// // console.log(greater)
 
-// Name with lenght more than 4
-// const names = ["Rahul", "Amit", "Naman", "Ram"];
-// const longNames = names.filter(n=> n.length>4)
-// console.log(longNames)
+// // Name with lenght more than 4
+// // const names = ["Rahul", "Amit", "Naman", "Ram"];
+// // const longNames = names.filter(n=> n.length>4)
+// // console.log(longNames)
 
-const student = [
-    { name: "Naman Singh", marks:89},
-    { name: "Deepak Yadav", marks:98},
-    { name: "Veepanshu Kasana", marks:43}
-]
+// // const student = [
+// //     { name: "Naman Singh", marks:89},
+// //     { name: "Deepak Yadav", marks:98},
+// //     { name: "Veepanshu Kasana", marks:43}
+// // ]
 
-const passedStudent = student.filter(stu => stu.marks >=50)
-console.log(passedStudent)
+// // const passedStudent = student.filter(stu => stu.marks >=50)
+// // console.log(passedStudent)
+
+// const products = [
+//     { name: "Laptop", price: 50000 },
+//     { name: "Mouse", price: 500 },
+//     { name: "Mobile", price: 20000 }
+// ]
+
+// const expensive = products.filter(
+//     prdt => prdt.price>1000
+// )
+
+// console.log(expensive)
+
+
+// const ages = [12, 18, 25, 15, 30];
+// const adults = ages.filter(age => age >= 18)
+// console.log("Can Vote", adults)
+
+// const array = ["Naman", "Aditya", "Veepanshu"]
+// const startA = array.filter(a => a[0]==="A")
+// console.log(startA)
+
+// Filter all emails that end with "@gmail.com".
+
+// const emails = ["naman@gmail.com", "veepanshu@yahoo.com", "deepak@gmail.com"]
+// const include = emails.filter(em=> em.includes("@gmail.com"))
+// console.log(include)
+
+// Filter all numbers that are multiples of both 2 and 3
+
+// const arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+// const divisible = arr.filter(n=> n%2==0 && n%3==0)
+// console.log(divisible)
+
+// Filter all strings that contain more than one word (contain a space).
+
+// const array = ["Naman Singh", "Aditya", "Veepanshu"]
+// const space = array.filter(s=> s.includes(" "))
+// console.log(space)
